cdbd010de77146c3d66534481ce916773453a821
mmoraisjr <jr.morais.marcos@gmail.com>
2022-08-21 02:26:38 -0300

    code refactoring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,60 @@
-const inputDescription = document.getElementById("description");
-const inputDate = document.getElementById("date");
-const inputTime = document.getElementById("time");
-const btn = document.getElementById("btn");
-const showDescription = document.querySelector(".show-description");
-const showDate = document.querySelector(".show-date");
-const showTime = document.querySelector(".show-time");
-
 const form = document.getElementById("add-item");
-const items = localStorage.getItem("items") || [];
+const items = JSON.parse(localStorage.getItem("items")) || [];
+const showResults = document.querySelector(".results");
 
-console.log(items);
-
-// btn.addEventListener("click", () => inputs());
-
-// const inputs = () => {
-//   description = inputDescription.value;
-//   date = inputDate.value;
-//   time = inputTime.value;
-
-//   showDescription.innerHTML = description;
-//   showDate.innerHTML = date;
-//   showTime.innerHTML = time;
-// };
+//Percorre a lista de objetos já criados e executa a função de criação de item na tela
+items.forEach((i) => {
+  addSchedule(i);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  
+  //dentro do target do form, seleciona os elementos de cada id do form
   const description = e.target.elements["description"];
   const date = e.target.elements["date"];
   const time = e.target.elements["time"];
 
-  addSchedule(description, date, time);
+  //a partir do valor inserido nos inputs do form, cria um objeto com os atributos correspondentes
+  const item = {
+    description: description.value,
+    date: date.value,
+    time: time.value,
+  };
 
+  //executa a função de criar o item em tela
+  addSchedule(item);
+
+  //insere o item ao array dos itens
+  items.push(item);
+
+  //como o LocalStorage so aceita conteúdo em String, 
+  //o conteúdo do array é transformado em uma String para ser armazenado
+  localStorage.setItem("items", JSON.stringify(items));
+
+  //define o valor dos inputs para vazio, ao executar a inserção
   description.value = "";
   date.value = "";
   time.value = "";
-
-  console.log(items);
 });
 
-function addSchedule(description, date, time) {
-  const item = {
-    "description": description,
-    "date": date,
-    "time": time
-  }
+function addSchedule(item) {
+  const newDescription = document.createElement("p");
+  newDescription.classList.add("show-description");
+  const newDate = document.createElement("p");
+  newDate.classList.add("show-date");
+  const newTime = document.createElement("p");
+  newTime.classList.add("show-time");
 
-  items.push(item);
+  newDescription.innerHTML = item.description;
+  newDate.innerHTML = item.date;
+  newTime.innerHTML = item.time;
 
-  localStorage.setItem("items", JSON.stringify(items));
+  showResults.appendChild(newDescription);
+  showResults.appendChild(newDate);
+  showResults.appendChild(newTime);
+
+  console.log(item.description);
+  console.log(item.date.value);
+  console.log(item.time.value);
 }
